36da5d359aae90a2dc093ef79d8d139a6604566c
Melihcan Kazım Karaca <karacamelihcan2@gmail.com>
2020-06-24 01:04:14 +0300

    controller, model, viewmodel, view added

---
+++ TodoApplication/Models/TodoItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApplication.Models
+{
+    public class TodoItem
+    {
+        public Guid Id { get; set; }
+        public bool IsDone { get; set; }
+        public string Title { get; set; }
+        public DateTimeOffset? DueAt { get; set; }
+
+    }
+}
---
+++ TodoApplication/Models/TodoViewModel.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections.Generic;
+namespace TodoApplication.Models
+{
+    public class TodoViewModel
+    {
+        public IEnumerable<TodoItem> Items { get; set; }
+    }
+}
